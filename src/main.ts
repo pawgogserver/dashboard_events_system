0045b772791efc1dda01b12a4d653faa572f33fe
pawgog <pgogola10@gmail.com>
2026-02-08 20:57:28 +0100

    Add cross origin rule

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
+
+  app.enableCors({
+    origin: allowedOrigin,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
